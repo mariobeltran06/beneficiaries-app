493fd6027e5672e9fa5720ccbab1130161b97c90
Mario Beltran <mariobeltran0699@gmail.com>
2023-06-04 15:42:36 -0600

    ⚡️ update store

--- src/app/store/actions/register-beneficiary.actions.ts
+++ src/app/store/actions/register-beneficiary.actions.ts
@@ -5,3 +5,5 @@ export const saveData = createAction(
   '[Register Beneficiary] Save all data',
   props<{ beneficiaries: IBeneficiary[] }>()
 );
+
+export const removeData = createAction('[Admin Beneficiary] Delete data');
--- src/app/store/reducers/beneficiary.reducers.ts
+++ src/app/store/reducers/beneficiary.reducers.ts
@@ -1,8 +1,9 @@
 import { createReducer, on } from '@ngrx/store';
-import { saveData } from '../actions/register-beneficiary.actions';
+import { removeData, saveData } from '../actions/register-beneficiary.actions';
 import { beneficiaryInitialState } from '../states/beneficiary.states';
 
 export const beneficiaryReducers = createReducer(
   beneficiaryInitialState,
-  on(saveData, (state, { beneficiaries }) => ({ ...state, beneficiaries }))
+  on(saveData, (state, { beneficiaries }) => ({ ...state, beneficiaries })),
+  on(removeData, state => ({ ...state, beneficiaries: null }))
 );
