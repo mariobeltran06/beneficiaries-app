c4137462ac7dbd615d759a495a44752192248a95
Mario Beltran <mariobeltran0699@gmail.com>
2023-06-04 03:00:26 -0600

    ♻️ update register beneficiary module

--- src/app/modules/register-beneficiary/register-beneficiary.module.ts
+++ src/app/modules/register-beneficiary/register-beneficiary.module.ts
@@ -3,16 +3,32 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { FormBeneficiaryComponent } from './components/form-beneficiary/form-beneficiary.component';
 import { LayoutFormComponent } from './components/layout-form/layout-form.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { RegisterBeneficiaryRoutingModule } from './register-beneficiary-routing.module';
-import { FormBeneficiaryComponent } from './components/form-beneficiary/form-beneficiary.component';
 
-const THEME_MATERIAL = [MatCardModule, MatButtonModule, MatIconModule];
+const THEME_MATERIAL = [
+  MatCardModule,
+  MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatSelectModule,
+  MatInputModule,
+  MatDividerModule,
+];
 
 @NgModule({
-  declarations: [RegisterPageComponent, LayoutFormComponent, FormBeneficiaryComponent],
+  declarations: [
+    RegisterPageComponent,
+    LayoutFormComponent,
+    FormBeneficiaryComponent,
+  ],
   imports: [
     CommonModule,
     ReactiveFormsModule,
