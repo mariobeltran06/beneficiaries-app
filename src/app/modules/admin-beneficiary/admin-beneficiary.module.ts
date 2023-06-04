beaab6e1f5ace1aed949f89f221f576c98faa1c6
Mario Beltran <mariobeltran0699@gmail.com>
2023-06-04 15:37:03 -0600

    ♻️ refactor code on admin beneficiary module

--- src/app/modules/admin-beneficiary/admin-beneficiary.module.ts
+++ src/app/modules/admin-beneficiary/admin-beneficiary.module.ts
@@ -1,15 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTableModule } from '@angular/material/table';
 import { AdminBeneficiaryRoutingModule } from './admin-beneficiary-routing.module';
+import { LayoutTableComponent } from './components/layout-table/layout-table.component';
+import { TableBeneficiariesComponent } from './components/table-beneficiaries/table-beneficiaries.component';
 import { BeneficiariesPageComponent } from './pages/beneficiaries-page/beneficiaries-page.component';
+
+const THEME_MATERIAL = [
+  MatButtonModule,
+  MatCardModule,
+  MatTableModule,
+  MatDividerModule,
+  MatDialogModule,
+];
 
 @NgModule({
   declarations: [
-    BeneficiariesPageComponent
+    BeneficiariesPageComponent,
+    LayoutTableComponent,
+    TableBeneficiariesComponent,
   ],
-  imports: [
-    CommonModule,
-    AdminBeneficiaryRoutingModule
-  ]
+  imports: [CommonModule, AdminBeneficiaryRoutingModule, ...THEME_MATERIAL],
 })
-export class AdminBeneficiaryModule { }
+export class AdminBeneficiaryModule {}
