b36a0bdf6491a11863ae29c0ab989dfc6e3f9be9
Mario Beltran <mariobeltran0699@gmail.com>
2023-06-04 02:59:17 -0600

    🚧 add new features on components

--- src/app/modules/register-beneficiary/components/form-beneficiary/form-beneficiary.component.ts
+++ src/app/modules/register-beneficiary/components/form-beneficiary/form-beneficiary.component.ts
@@ -1,11 +1,71 @@
-import { Component, Input } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RELATIONSHIP_ITEMS } from 'src/app/modules/core/constants/relationship-items.constant';
+import { IRelashionshipItem } from 'src/app/modules/core/interfaces/relationship-item.interface';
+import { PATTERN_NAME } from 'src/app/modules/core/utils/patterns.util';
+import { validateTotalPercentage } from 'src/app/modules/core/validators/total-percentage.validator';
+import { IBeneficiary } from '../../interfaces/beneficiary.interface';
 
 @Component({
   selector: 'app-form-beneficiary',
   templateUrl: './form-beneficiary.component.html',
-  styleUrls: ['./form-beneficiary.component.scss']
+  styleUrls: ['./form-beneficiary.component.scss'],
 })
 export class FormBeneficiaryComponent {
-  @Input() formBeneficiary!: FormGroup;
+  formBeneficiary: FormGroup;
+  itemsRelationship: IRelashionshipItem[] = RELATIONSHIP_ITEMS;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formBeneficiary = this.formBuilder.group({
+      beneficiaries: this.formBuilder.array(
+        [],
+        validateTotalPercentage('percentage')
+      ),
+    });
+  }
+
+  get beneficiaries(): FormArray {
+    return this.formBeneficiary.get('beneficiaries') as FormArray;
+  }
+
+  get arrayBeneficiariesLength(): number {
+    return (this.formBeneficiary.get('beneficiaries') as FormArray).length;
+  }
+
+  get totalPercentageError(): boolean | null {
+    return this.formBeneficiary.get('beneficiaries')?.errors?.['totalPercentageInvalid'];
+  }
+
+  addBeneficiary(): void {
+    const beneficiaryFormGroup = this.formBuilder.nonNullable.group({
+      fullname: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(PATTERN_NAME),
+          Validators.minLength(2),
+          Validators.maxLength(50),
+        ],
+      ],
+      relationship: [this.itemsRelationship[0].value, [Validators.required]],
+      percentage: [
+        1,
+        [Validators.required, Validators.min(1), Validators.max(100)],
+      ],
+    });
+    this.beneficiaries.push(beneficiaryFormGroup);
+  }
+
+  removeBeneficiary(index: number): void {
+    this.beneficiaries.removeAt(index);
+  }
+
+  saveForm(): void {
+    if(this.beneficiaries.valid){
+      const beneficiaries: IBeneficiary[] = {
+        ...this.formBeneficiary.value
+      }
+      console.log(beneficiaries);
+    }
+  }
 }
--- src/app/modules/register-beneficiary/components/layout-form/layout-form.component.ts
+++ src/app/modules/register-beneficiary/components/layout-form/layout-form.component.ts
@@ -1,44 +1,8 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-layout-form',
   templateUrl: './layout-form.component.html',
   styleUrls: ['./layout-form.component.scss'],
 })
-export class LayoutFormComponent {
-  layoutForm: FormGroup;
-
-  constructor(private formBuilder: FormBuilder) {
-    this.layoutForm = this.formBuilder.group({
-      beneficiaries: this.formBuilder.array([]),
-    });
-  }
-
-  get beneficiaries(): FormArray {
-    return this.layoutForm.get('beneficiaries') as FormArray;
-  }
-
-  addBeneficiary() : void {
-    const beneficiaryFormGroup = this.formBuilder.nonNullable.group({
-      fullname: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(2),
-          Validators.maxLength(50),
-        ],
-      ],
-      relationship: ['', [Validators.required]],
-      percentage: [
-        1,
-        [Validators.required, Validators.min(1), Validators.max(100)],
-      ],
-    });
-    this.beneficiaries.push(beneficiaryFormGroup);
-  }
-
-  removeBeneficiary(index: number) : void {
-    this.beneficiaries.removeAt(index);
-  }
-}
+export class LayoutFormComponent {}
